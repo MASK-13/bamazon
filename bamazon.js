a640511309861b908b866733a1e1e0bfc000c731
Mike Morales <mikemorale@live.com>
2018-02-05 12:14:39 -0700

    product

--- bamazon.js
+++ bamazon.js
@@ -1,39 +1,217 @@
+var mysql = require('mysql');
 
-const mysql = require('mysql');
-
-const inquirer = require('inquirer');
-
-
-
-const chalk = require('chalk');
-
-const Table = require('cli-table-redemption');
-
-
-
-let userStatus, itemIndex;
+var inquirer = require('inquirer');
 ///// 
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: "localhost",
     port: 3306,
-    user: 'root',
-    password: 'dkamka13',
-    database: 'bamazon',
+    user: "root",
+    password: "dkamka13",
+    database: "bamazon",
+});
+
+connection.connect(function(err) {
+
+    console.log("Connected as id: " + connection.threadId);
+    console.log('Welcome to Bamazon!');
+
+    shop();
+
 });
 
 
+var shop = function() {
 
-function connectToDB(){
+    connection.query("SELECT * FROM products", function(err, res) {
 
-  connection.connect(function(err){
+        //Presents user with a list of items available for purchase. Loops through the products
 
-    if (err) throw err;
+        //table and pushes items into the productsArray.
 
-    console.log('connected to DB');
+        inquirer.prompt({
 
-    displayWelcome();
+            name: "products",
 
-  });
+            type: "list",
+
+            choices: function(value) {
+
+                var productsArray = [];
+
+                for (var i = 0; i < res.length; i++) {
+
+                    productsArray.push(res[i].product_name);
+
+                }
+
+                return productsArray;
+
+            },
+
+            message: "Which product you would like to buy?"
+
+        }).then(function(answer) {
+
+            for (var i = 0; i < res.length; i++) {
+
+                //Finds a match with the user's answer...
+
+                if (res[i].product_name === answer.products) {
+
+                    var chosenItem = res[i];
+
+                    inquirer.prompt({
+
+                        name: "quantity",
+
+                        type: "input",
+
+                        message: "How many ?"
+
+                    }).then(function(answer) {
+
+                        //if insufficient stock, prompts user to try again
+
+                        if (chosenItem.stock_quantity < parseInt(answer.quantity)) {
+
+                            console.log("Sorry, we only have " + chosenItem.stock_quantity + " of those in stock.");
+
+                            shop();
+
+                            //if sufficient product, allows user to purchase item(s). Updates products table
+
+                        } else {
+
+                            var total = chosenItem.price * parseInt(answer.quantity);
+
+
+
+                            connection.query("UPDATE products SET ? WHERE ?", [{
+
+                                stock_quantity: chosenItem.stock_quantity - parseInt(answer.quantity),
+
+                                product_sales: chosenItem.product_sales + total
+
+                            }, {
+
+                                item_id: chosenItem.item_id
+
+                            }], function(err, res) {
+
+
+
+                            });
+
+                            //
+
+                            connection.query("SELECT total_sales,department_name FROM departments", function(err, res) {
+
+                                for (var i = 0; i < res.length; i++) {
+
+                                    if (res[i].department_name === chosenItem.department_name) {
+
+                                        var prevTotal = res[i].total_sales;
+
+                                        var sumSales = prevTotal + total;
+
+                                        var departmentName = chosenItem.department_name;
+
+
+
+                                        connection.query("UPDATE departments SET ? WHERE ?", [{
+
+
+
+                                            total_sales: sumSales
+
+                                        }, {
+
+                                            department_name: departmentName
+
+
+
+                                        }])
+
+                                    }
+
+                                }
+
+                            })
+
+
+
+                            const maybePluralize = (count, noun, suffix = 's') =>
+
+                                `${count} ${noun}${count !== 1 && noun.charAt(noun.length-1)!== 's'? suffix : ''}`;
+
+                            console.log("You have bought " + maybePluralize(parseInt(answer.quantity), chosenItem.product_name));
+
+                            console.log("Your total is $" + total);
+
+                            taskChoice();
+
+                        }
+
+                    })
+
+                }
+
+
+
+            }
+
+        })
+
+    })
 
 }
 
+
+
+function taskChoice() {
+
+    inquirer.prompt([{
+
+
+
+            type: 'list',
+
+            name: 'task',
+
+            message: 'What would you like to do?',
+
+            choices: ['buy-stuff', 'quit']
+
+        }
+
+
+
+    ]).then(function(choice) {
+
+
+
+        if (choice.task === 'buy-stuff') {
+
+            buyStuff();
+
+
+
+        } else {
+
+            console.log('Thank YOU ! please come back soon and buy');
+
+            connection.end(function(err) {
+
+
+
+
+
+            });
+
+
+
+        }
+
+    });
+
+}
--- bamazon.sql
+++ bamazon.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE IF NOT EXIST bamazon;
-
 CREATE DATABASE bamazon;
 
 USE bamazon db;
@@ -7,42 +5,31 @@ USE bamazon db;
 DROP TABLE products;
 
 CREATE TABLE products (
-  item_id INT NOT NULL PRIMARY KEY auto_increment,
+  item_id INT NOT NULL auto_increment,
 
-  product_name VARCHAR (100) NULL,
+  product_name VARCHAR (200) NULL,
 
-  department_name VARCHAR (100) NULL,
+  department_name VARCHAR (200) NULL,
 
-  price DECIMAL (10,2) NULL,
+  price DECIMAL (10,2) NOT NULL DEFAULT 0,
 
   stock_quantity INT NULL,
 
-  product_sales DECIMAL (10,2) NULL
-
 );
 
 
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity, product_sales) 
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity ) 
 VALUES 
-( 123, 'BRIDGE BURNER', "MISCELLENEOUS", 99.97, 2, 90 ), 
-( 231, 'TIMETRAVEL MACHINE', "GADGETS", 1.23, 1, 1001),
-( 546, 'MAGIC FLUKE', "MUSIC", 1.99. 5, 2.02), 
-( 653, "ROCKETSHIP", "TRANSPORT", 1236.02, 1 , 52653.00),
-( 555, "LUCK CHARM", "TRICKERY", 25.23, 99, 111.02),
-( 111, "FLYING BRRROOM", "TRANSPORT", 22.33, 3 ,23.33),
-( 656, "WORMS", "FISHING", 0.99, 99, 101.99),
-( 717, "DIAMOND BLADE", "GADGETS" 33.35, 4, 55.23),
-( 987, "SPYWARE", "GADGETS", 99.11, 1, 33.99),
-( 010, "BITCOINS", "MISCELLENEOUS" 111.23, 9 , 999.00);
-
-CREATE TABLE deparments(
-	department_id INT NOT NULL PRIMARY KEY auto_increment,
-	department_name VARCHAR (100) NULL,
-	expenses DECIMAL (10,4) NULL
-);
+( 123, 'BRIDGE BURNER', "MISCELLENEOUS", 99.97, 2 ), 
+( 231, 'TIMETRAVEL MACHINE', "GADGETS", 1.23, 1),
+( 546, 'MAGIC FLUKE', "MUSIC", 1.99, 5), 
+( 653, "ROCKETSHIP", "TRANSPORT", 1236.02,1),
+( 555, "LUCK CHARM", "TRICKERY", 25.23, 99),
+( 111, "FLYING BRRROOM", "TRANSPORT", 22.33, 3),
+( 656, "WORMS", "FISHING", 0.99, 99),
+( 717, "DIAMOND BLADE", "GADGETS", 33.35, 4),
+( 987, "SPYWARE", "GADGETS", 99.11, 1),
+( 010, "BITCOINS", "MISCELLENEOUS", 111.23, 9);
 
 
-DROP TABLE departments;
-
-INSERT INTO  departments (department_name,expenses)
 
